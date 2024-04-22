e656a399ab1d4336b6f182fc0291e3c4ff2f88ac
Ai-Imran <ikimranahmed00143@gmail.com>
2024-04-22 17:39:22 +0600

    userstart add payment details

--- src/Pages/UserStartService.jsx
+++ src/Pages/UserStartService.jsx
@@ -1,6 +1,10 @@
 
 const UserStartService = () => {
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="text-white px-3 min-h-screen">
             <div className="lg:mx-auto mt-8 text-center">
@@ -11,7 +15,7 @@ const UserStartService = () => {
                 তাই আর দেরি কিসের পছন্দ অনুযায়ী গাড়ি সিলেক্ট করে, সবগুলো তথ্য দিয়ে শুধু একবার যোগাযোগ করুন। আমরা আপনার সুবিধা অনুযায়ী যেকোনো সার্ভিস দিতে এগিয়ে যাবো সবসময়
                 </p>
             </div>
-            <form className="lg:w-1/2  mx-auto my-8">
+            <form onSubmit={handleSubmit} className="lg:w-1/2  mx-auto my-8">
                 <div className="my-2">
                 <label className="font-bold" htmlFor="name">আপনার নাম লিখুন</label>
                 <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="text" name="name" placeholder="এখানে আপনার নাম লিখুন" />
@@ -56,10 +60,10 @@ const UserStartService = () => {
                 <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="car-number" id="car-number" placeholder="গাড়ির সংখ্যা লিখুন" />
                  </div>
                  <div className="my-2">  
-                <label className="font-bold" htmlFor="my-work">আপনার পেশা / আপনি কি কাজ করেন?</label>              
+                <label className="font-bold" htmlFor="my-work">আপনার পেশা</label>              
                 <select className="bg-gray-700  rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-10 py-2 block " id="my-work" name="my-work">
-                <option value="অটো গাড়ি">অটো গাড়ি</option>
-                <option value="রিক্সা">রিক্সা</option>              
+                <option value="অটো গাড়ি">ছাত্র/ছাত্রী </option>
+                <option value="সাধারণ যাত্রী">সাধারণ যাত্রী</option>
                 </select>
                 </div>
                
@@ -77,12 +81,12 @@ const UserStartService = () => {
                 <label className="font-bold" htmlFor="student">তাদের পেশা</label>              
                 <select className="bg-gray-700  rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-10 py-2 block " id="student" name="student">
                 <option value="অটো গাড়ি">ছাত্র/ছাত্রী </option>
-                             
+                
                 </select>
                 </div>
                 <div className="my-2">
                 <label className="font-bold" htmlFor="student-number">ছাত্র/ছাত্রী সংখ্যা</label>
-                <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="student-number" id="student-number" placeholder="ছাত্র/ছাত্রী সংখ্যা লিখুন" />
+                <input  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="student-number" id="student-number" placeholder="ছাত্র/ছাত্রী সংখ্যা লিখুন" />
                  </div>
                 </div>
                 <p className="text-yellow-300 mt-2">
@@ -97,9 +101,26 @@ const UserStartService = () => {
                 </div>
                 <div className="my-2">
                 <label className="font-bold" htmlFor="public-number">যাত্রী সংখ্যা</label>
-                <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="public-number" id="public-number" placeholder="যাত্রী সংখ্যা লিখুন" />
+                <input  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="public-number" id="public-number" placeholder="যাত্রী সংখ্যা লিখুন" />
                  </div>
                 </div>
+
+                <div className="bg-white text-xl text-black px-4 rounded py-4">
+                    <h3 className="mx-auto  text-center">আপনার হিসাব-নিকাশ</h3>
+                    <div className="flex justify-between">
+                        <p>মোট যাত্রীসংখ্যা</p> <span>0</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <p>সার্ভিসজনিত ট্যাক্স/ভ্যাট</p> <span>0 টাকা</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <p>অন্যান্য খরচ</p> <span>0 টাকা</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <p>সর্বমোট খরচ</p> <span>0 টাকা</span>
+                    </div>
+                </div>
+
                 <div className="mx-auto  text-center">
                     <div className='bg-gradient-to-r from-purple-600 via-red-600 mx-auto text-center  to-yellow-300 mt-5 p-2 rounded-md font-semibold cursor-pointer '>
                  <input className=' lg:w-[350px] text-center mx-auto font-bold text-white cursor-pointer' type="submit" value="সাবমিট করুন" />

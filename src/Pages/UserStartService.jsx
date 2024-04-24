9e21327e11df552ad5d1f13f18babaeb71ca7e1b
Ai-Imran <ikimranahmed00143@gmail.com>
2024-04-24 17:48:05 +0600

    adding price by place

--- src/Pages/UserStartService.jsx
+++ src/Pages/UserStartService.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
-
-
-
-   
-
+import { useState,useEffect } from "react"
 
 const UserStartService = () => {
-    const [selectedOption, setSelectedOption] = useState('সাধারণ যাত্রী');
-    const publicPrice = 15;
-    const studentPrice = 10;
+    const [studentPrice, setStudentPrice] = useState(0);
+    const [publicPrice, setPublicPrice] = useState(0);
+    const taxPrice = 5;
+    const [totalPassenger, setTotalPassenger] = useState(0);
+    const [selectedPlace, setSelectedPlace] = useState("");
 
-    const handleSelectChange = (e) => {
-        setSelectedOption(e.target.value);
-      };
+    const handlePlaceChange = (e) => {
+        const selectedValue = e.target.value;
+        setSelectedPlace(selectedValue);
+
+        // Update studentPrice based on selectedValue
+        if (selectedValue === "homna to goaribhanga") {
+            setStudentPrice(10);
+            setPublicPrice(15)
+        } 
+        else if (selectedValue === "homna to batakandi") {
+            setStudentPrice(20);
+            setPublicPrice(30)
+        } 
+        
+        else {
+            // Reset studentPrice if a different option is selected
+            setStudentPrice(0);
+            setPublicPrice(0)
+        }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -30,7 +44,7 @@ const UserStartService = () => {
             </div>
             <form onSubmit={handleSubmit} className="lg:w-1/2  mx-auto my-8">
                 <div className="my-2">
-                <label className="font-bold" htmlFor="name">আপনার নাম </label>
+                <label className="font-bold" htmlFor="name">আপনার নাম লিখুন</label>
                 <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="text" name="name" placeholder="এখানে আপনার নাম লিখুন" />
                  </div>
                  <div className="my-2">
@@ -39,11 +53,14 @@ const UserStartService = () => {
                  </div>
                  <div className="my-2">
                 <label className="font-bold" htmlFor="email">আপনার ইমেইল লিখুন</label>
-                <input  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="email" name="email" placeholder="এখানে আপনার ইমেইল লিখুন" />
+                <input required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="email" name="email" placeholder="এখানে আপনার ইমেইল লিখুন" />
                  </div>
                 <div className="my-2">  
                 <label className="font-bold" htmlFor="targetplace">আপনি কোথায় থেকে কোথায় যেতে চান</label>              
-                <select required className="bg-gray-700  rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-10 py-2 block " id="targetplace" name="targetplace">
+                <select onChange={handlePlaceChange} className="bg-gray-700  rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-10 py-2 block " id="targetplace" name="targetplace">
+                
+                <option value="null">এখানে ক্লিক করুন </option>
+
                 <option value="homna to goaribhanga">হোমনা থেকে গোয়ারিভাংঙ্গা </option>
                 <option value="homna to batakandi">হোমনা থেকে বাতাকান্দি</option>              
                 </select>
@@ -68,31 +85,20 @@ const UserStartService = () => {
                 </select>
                 </div>
                
-                <div className="my-2">
-                <label className="font-bold" htmlFor="car-number">আপনার কয়টি গাড়ি প্রয়োজন</label>
-                <input defaultValue={1} required className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="car-number" id="car-number" placeholder="গাড়ির সংখ্যা লিখুন" />
-                 </div>
+               <div className="flex gap-5">
                  <div className="my-2">  
-      <label className="font-bold" htmlFor="my-work">আপনার পেশা</label>              
-      <div className="flex gap-10 items-center">
-        <div>
-          <select 
-            className="bg-gray-700 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-5 py-2 block" 
-            id="my-work" 
-            name="my-work"
-            value={selectedOption}
-            onChange={handleSelectChange}
-          >
-            <option value="ছাত্র/ছাত্রী">ছাত্র/ছাত্রী</option>
-            <option value="সাধারণ যাত্রী">সাধারণ যাত্রী</option>
-          </select>
-        </div>
-        <p className="mx-2 text-red-400">
-          আপনার ভাড়া : 
-          {selectedOption === 'ছাত্র/ছাত্রী' ? studentPrice : publicPrice} টাকা
-        </p>
-      </div>
-    </div>
+                <label className="font-bold" htmlFor="my-work">আপনার পেশা</label>              
+                <select className="bg-gray-700  rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-10 py-2 block " id="my-work" name="my-work">
+                <option value="অটো গাড়ি">ছাত্র/ছাত্রী </option>
+                <option value="সাধারণ যাত্রী">সাধারণ যাত্রী</option>
+                </select>
+                </div>
+               <div className="my-2">
+                <label className="font-bold" htmlFor="i-am">যাত্রী সংখ্যা</label>
+                <input value={1} readOnly  className="bg-gray-700 w-1/2 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="i-am" id="i-am" placeholder="1" />
+                 </div>
+               
+               </div>
                
                 <p className="text-yellow-300">
                 আপনারা যদি একাধিক যাত্রী হন তাহলে এগুলো পূরণ করতে হবে*
@@ -100,7 +106,7 @@ const UserStartService = () => {
                 <p>
                 তাদের পেশা ও যাত্রী সংখ্যা সিলেক্ট করুন
                 </p>
-                
+              
                 <div className="flex gap-3 lg:gap-8">
                 <div className="">  
                 <label className="font-bold" htmlFor="student">তাদের পেশা</label>              
@@ -111,7 +117,7 @@ const UserStartService = () => {
                 </div>
                 <div className="my-2">
                 <label className="font-bold" htmlFor="student-number">ছাত্র/ছাত্রী সংখ্যা</label>
-                <input defaultValue={0}  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="student-number" id="student-number" placeholder="ছাত্র/ছাত্রী সংখ্যা লিখুন" />
+                <input  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="student-number" id="student-number" placeholder="ছাত্র/ছাত্রী সংখ্যা লিখুন" />
                  </div>
                 </div>
                 
@@ -124,27 +130,27 @@ const UserStartService = () => {
                 </div>
                 <div className="my-2">
                 <label className="font-bold" htmlFor="public-number">যাত্রী সংখ্যা</label>
-                <input defaultValue={0}  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="public-number" id="public-number" placeholder="যাত্রী সংখ্যা লিখুন" />
+                <input  className="bg-gray-700 w-11/12 rounded-md text-white focus:border-lime-500 focus:border focus:shadow-lg outline-none px-3 py-2 block " type="number" name="public-number" id="public-number" placeholder="যাত্রী সংখ্যা লিখুন" />
                  </div>
                 </div>
 
                 <div className="bg-white text-xl text-black px-4 rounded py-4">
                     <h3 className="mx-auto  text-center">আপনার হিসাব-নিকাশ</h3>
                     <div className="flex justify-between">
+                    <p className=" ">
+                    একজন সাধারণ যাত্রী ভাড়া 
+                    </p> <span>{publicPrice} টাকা</span>
+                    </div>
+                     <div className="flex justify-between">
                     <p className="">
-                    একজন সাধারণ যাত্রীর ভাড়া
-                </p> <span>{publicPrice} টাকা</span>
+                    একজন ছাত্র/ছাত্রীর ভাড়া 
+                    </p> <span> {studentPrice} টাকা</span>
                     </div>
                     <div className="flex justify-between">
-                    <p className="">
-                একজন ছাত্র/ছাত্রীর ভাড়া 
-                </p> <span>{studentPrice} টাকা</span>
+                        <p>মোট যাত্রীসংখ্যা</p> <span>{totalPassenger} জন</span>
                     </div>
                     <div className="flex justify-between">
-                        <p>মোট যাত্রীসংখ্যা</p> <span>0</span>
-                    </div>
-                    <div className="flex justify-between">
-                        <p>সার্ভিসজনিত ট্যাক্স/ভ্যাট</p> <span>0 টাকা</span>
+                        <p>সার্ভিসজনিত ট্যাক্স/ভ্যাট</p> <span>{taxPrice} টাকা</span>
                     </div>
                     <div className="flex justify-between">
                         <p>অন্যান্য খরচ</p> <span>0 টাকা</span>

e220b530f7e44d0185920706a991c63c3b63046f
Ai-Imran <ikimranahmed00143@gmail.com>
2024-04-27 17:27:19 +0600

    complete show image

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -7,6 +7,7 @@ import { BsSendFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa6";
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
+import RandomColorText from "./RandomColorText";
 
 const Navbar = () => {
 
@@ -24,16 +25,28 @@ const Navbar = () => {
                      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
                      <div className="drawer-content">
                        {/* Page content here */}
-                       <img className="text-white  absolute" src="" alt="fdfdf" />
-{/* 
-                       <label htmlFor="my-drawer-4" className="drawer-button text-white text-4xl ">  <RiMenuUnfoldLine className="ml-[140px]"/></label> */}
+                       <div className=" ml-[165px]">
+                       {
+                            user && <>
+                             <img className="text-white w-[60px] h-[60px] border shadow-2xl border-purple-400 rounded" src={user?.photoURL} alt="user Photo" />
+                            </>
+                        }
+                       </div>
+
                      </div> 
                      <div className="drawer-side">
                        <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                        
                        <ul className="menu backimage space-y-6 p-4 w-3/4 min-h-full text-xl font-bold text-base-content">
                          {/* Sidebar content here */}
-                        <img className="text-white" src="" alt="fdfdf" />
+                        <div className="text-white">
+                       {
+                        user && <>
+                         <img className="text-white rounded-sm  mx-auto" src={user?.photoURL} alt="user photo" />
+                         <p className="text-lime-400">{user?.displayName}</p>
+                        </>
+                       }
+                        </div>
                         <NavLink to={'/'} ink="true">হোম</NavLink>
                         {
                             user ? <NavLink to={'/user-start'}>শুরু করুন</NavLink> : <NavLink to={'/startSerives'}>সার্ভিস দেখুন</NavLink>
@@ -50,11 +63,12 @@ const Navbar = () => {
                      </div>
                      
                 </div>
+                {/* <div className="border p-2 border-dashed ml-3 text-shadow rounded-full border:animate-spin"> <RandomColorText className="bg-green-500 " text={'0'}/> </div> */}
 
-                    <Link to={'/'} className=" text-lime-500 hover:shadow-xl hover:text-lime-300 text-3xl">IsmaqW</Link>
+                    <Link to={'/'} className=" ml-3 rounded font-serif text-stone-300 px-1 text-shadow hover:text-lime-300 shad text-3xl" >Ismaqw</Link>
                 </div>
                 <div className="navbar-center  hidden lg:flex">
-                    <ul className="menu space-x-4 text-[18px] ml-10 font-bold menu-horizontal px-1">
+                    <ul className="menu space-x-4 text-[18px]  ml-10 font-bold menu-horizontal px-1">
                        <NavLink to={'/'} ink="true">হোম</NavLink>
                        <NavLink to={'/user-start'}>শুরু করুন</NavLink>
                        <NavLink to={'/startSerives'}>সার্ভিস দেখুন</NavLink>
@@ -66,7 +80,9 @@ const Navbar = () => {
                        {
                         user ? <span onClick={handleLogout} className="text-yellow-500">Logout</span> :  <NavLink to={'/login'}>লগইন করুন</NavLink>
                        }
-
+                        {
+                            user && <img className="text-white w-[60px] h-[60px] -mt-5 rounded-full" src={user?.photoURL} alt="user Photo" />
+                        }
                        </ul>
                 </div>
                
